9b073aefc13a8414ee4817785c77dabd24ab226c
Sherzod Artikbayev <info@web-pro.uz>
2026-01-23 20:59:56 +0500

    feat: set up

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -1,5 +1,6 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import safeql from '@ts-safeql/eslint-plugin/config';
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -16,6 +17,11 @@ export default tseslint.config(
     files: ['**/*.{js,cjs,mjs}'],
     extends: [tseslint.configs.disableTypeChecked],
   },
+  safeql.configs.connections({
+    databaseUrl: 'postgres://postgres:admin@localhost:5432/postgres',
+    overrides: { types: { uuid: 'string' } },
+    targets: [{ wrapper: 'client.query' }],
+  }),
   {
     rules: {
       '@typescript-eslint/no-unused-vars': [
